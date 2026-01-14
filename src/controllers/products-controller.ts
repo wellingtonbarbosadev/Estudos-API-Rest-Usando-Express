fcaf53bc40f8925705e89344bca5cab4e580a4f3
Wellington Barbosa <huehueb58@gmail.com>
2026-01-14 15:12:20 -0300

    feat: aprimora validação no método create do ProductsController utilizando Zod

--- src/controllers/products-controller.ts
+++ src/controllers/products-controller.ts
@@ -21,27 +21,21 @@ export class ProductsController {
 
   create(request: Request, response: Response) {
     const bodySchema = z.object({
-      name: z.string(),
-      price: z.number(),
+      name: z
+        .string({
+          error: (err) => (!err.input ? "Name is required." : undefined),
+        })
+        .trim()
+        .min(6, { error: "Name must be at least 6 characters long." }),
+
+      price: z
+        .number({
+          error: (err) => (!err.input ? "Price is required." : undefined),
+        })
+        .positive({ error: "Price must be at positive." }),
     });
 
     const { name, price } = bodySchema.parse(request.body);
-
-    // if (!name) {
-    //   throw new AppError("The product name is required");
-    // }
-
-    // if (name.trim().length < 6) {
-    //   throw new AppError("The product name must be at least 6 characters long");
-    // }
-
-    // if (!price) {
-    //   throw new AppError("The product price is required");
-    // }
-
-    // if (price < 0) {
-    //   throw new AppError("The product price must be a positive number");
-    // }
 
     response.status(201).json({ name, price, user_id: request.user_id });
   }
