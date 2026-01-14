4081f3f715247fe935028039152dca00804efb12
Wellington Barbosa <huehueb58@gmail.com>
2026-01-14 13:42:18 -0300

    feat: atualiza validação no método create do ProductsController para exigir nome e preço

--- src/controllers/ProductsController.ts
+++ src/controllers/ProductsController.ts
@@ -20,8 +20,8 @@ export class ProductsController {
 
   create(request: Request, response: Response) {
     const { name, price } = request.body;
-    if (!name) {
-      throw new AppError("The product name is required");
+    if (!name || !price) {
+      throw new AppError("The product name and price are required");
     }
     response.status(201).json({ name, price, user_id: request.user_id });
   }
